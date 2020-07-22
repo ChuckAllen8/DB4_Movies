b26b5778fc8ca03fab38765e968e9d4353816aa8
Chuck Allen <ChuckAllen@QuickenLoans.com>
2020-07-22 15:02:52 -0400

    The initial commit of the project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DB4_Movies
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
